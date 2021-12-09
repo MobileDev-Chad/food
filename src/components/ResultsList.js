14049dc46d8a9d4226a974c3eb1fef63839f35de
WebDev-Chad <chadwin.allotey@gmail.com>
2021-12-09 01:50:26 -0500

    Update ResultsList.js

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,22 +1,27 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
-const ResultsList = ({title}) => {
- 
-
+const ResultsList = ({ title, results }) => {
   return (
     <View>
-<Text style={styles.titleStyle}>{title}</Text>
+      <Text style={styles.titleStyle}>{title}</Text>
+      <FlatList
+        keyExtractor={(result) => result.id}
+        horizontal
+        data={results}
+        renderItem={({ item }) => {
+          return <Text>{item.name}</Text>;
+        }}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    titleStyle: {
-        fontSize: 18,
-        fontWeight: 'bold'
-    }
-
+  titleStyle: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
 });
 
 export default ResultsList;
