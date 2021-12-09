1285623ab907345613384dd1b576910711b07ee5
WebDev-Chad <chadwin.allotey@gmail.com>
2021-12-09 01:42:03 -0500

    update

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
   const [query, setQuery] = useState("");
   const [searchApi, results, errorMessage] = useResults();
+
+  const filterReultsByPrice = (price) => {
+      // price === '$' || '$$' || '$$$'
+      return results.filter(result => {
+          return result.price === price;
+      })
+   }
 
   return (
     <View>
@@ -16,6 +24,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
+      <ResultsList results = {filterReultsByPrice('$')} title= "Cost Effective"/>
+      <ResultsList results = {filterReultsByPrice('$$')} title= "Bit Pricier"/>
+      <ResultsList results = {filterReultsByPrice('$$$')} title= "Big Spender"/>
     </View>
   );
 };
